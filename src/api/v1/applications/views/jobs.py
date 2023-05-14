fd91cfd896f1277390c7bcda34851b4c9e13f995
Vaipik <n.khylko@gmail.com>
2023-05-14 10:55:58 +0300

    permissions fix

--- src/api/v1/applications/views/jobs.py
+++ src/api/v1/applications/views/jobs.py
@@ -9,4 +9,4 @@ class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.select_related("header").all()
     serializer_class = JobSerializer
     http_method_names = ["get", "post", "put", "delete"]
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
--- src/config/settings.py
+++ src/config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY") or 123
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -122,6 +122,6 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'  # All endpoints should be accessible only to authenticated users.
     ]
 }
