0ed345a1a758e6ec34241afe90ce881cba533558
Vaipik <n.khylko@gmail.com>
2023-05-14 14:22:16 +0300

    urls.py added application name

--- src/api/v1/applications/urls.py
+++ src/api/v1/applications/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from ..applications.views import JobViewSet, UserApplicationGet
+from .views import JobViewSet, UserApplicationGet
 
 
 job_router = SimpleRouter()
-job_router.register(prefix="jobs", viewset=JobViewSet, basename="job")
+job_router.register(prefix="jobs", viewset=JobViewSet, basename="jobs")
 
+app_name = "applications"
 
 urlpatterns = [
     path("", include(job_router.urls)),
     path(
         r"users/<int:pk>/applications/", UserApplicationGet.as_view(), name="user_applications_list")
 ]
+
