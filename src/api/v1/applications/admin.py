3062a93cb9b518a987bc1b76a64ec1a0b25389e4
Vaipik <n.khylko@gmail.com>
2023-05-14 10:52:19 +0300

    prettifying admin view

--- src/api/v1/applications/admin.py
+++ src/api/v1/applications/admin.py
@@ -4,8 +4,33 @@ from django.contrib import admin
 from .models import Application, Job, JobHeader
 
 
-admin.site.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
 
-admin.site.register(Job)
+    list_display = ('id', 'job', 'user')
+    list_display_links = ('job', )
+    search_fields = ('job', 'user')
+    list_filter = ('job', 'user')
 
-admin.site.register(JobHeader)
+
+admin.site.register(Application, ApplicationAdmin)
+
+
+class JobAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "name", "type")
+    list_display_links = ("id", "name", "type")
+    search_fields = ("name", "type")
+    list_filter = ("name", "type")
+
+
+admin.site.register(Job, JobAdmin)
+
+
+class JobHeaderAdmin(admin.ModelAdmin):
+    list_display = ("id", "rich_title_text", "rich_subtitle_text", "job")
+    list_display_links = ("id", "rich_title_text", "rich_subtitle_text", "job")
+    search_fields = ("rich_title_text", "rich_subtitle_text", "job")
+    list_filter = ("rich_title_text", "rich_subtitle_text", "job")
+
+
+admin.site.register(JobHeader, JobHeaderAdmin)
